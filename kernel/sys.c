ff3370a1dcb9ced7c457ec073e9f8591c1b5c9cc
ahmedhanbal <ahmed.alizahid14@gmail.com>
2026-02-24 22:47:14 +0500

    integrate susfs

--- kernel/sys.c
+++ kernel/sys.c
@@ -1273,23 +1273,27 @@ static int override_release(char __user *release, size_t len)
 #endif
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 #ifndef CONFIG_FAKE_UNAME_NONE
-       if (current_uid().val == 0) {
-               if (!strncmp(current->comm, "bpfloader", 9) ||
-                       !strncmp(current->comm, "netbpfload", 10) ||
-                       !strncmp(current->comm, "netd", 4) ||
-                       !strncmp(current->comm, "uprobestats", 11)) {
-                       strcpy(tmp.release, FAKE_UNAME);
-                       pr_info("fake uname: %s/%d release=%s\n",
-                               current->comm, current->pid, tmp.release);
-               }
-       }
+    if (current_uid().val == 0) {
+        if (!strncmp(current->comm, "bpfloader", 9) ||
+            !strncmp(current->comm, "netbpfload", 10) ||
+            !strncmp(current->comm, "netd", 4) ||
+            !strncmp(current->comm, "uprobestats", 11)) {
+            strcpy(tmp.release, FAKE_UNAME);
+        }
+    }
 #endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
